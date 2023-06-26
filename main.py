c8b86088175f3a88c18aafc405785ecd346e4749
lasergraph <118357000+lasergraph@users.noreply.github.com>
2023-06-26 16:16:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import subprocess
 from datetime import datetime
 import time
 import requests
-import math
 
 #Variabeln
 url = "http://192.168.1.231/rest/external/http/alarm/v2"
-now = datetime.now()
-refdates = [[2022, 1, 31], [2022, 2, 7], [2022, 2, 14], [2022, 2, 21], [2022, 2, 28], [2022, 3, 7]]
 
 #Prüfen ob SMS vorhanden sind.
 def get_sms_used():
@@ -21,7 +18,6 @@ def get_sms_used():
 			else:
 				return True
                
-
 #Neue SMS abrufen
 def get_new_sms():
 	rcmd = subprocess.check_output(["gsmctl", "-S", "-r", "0"])
@@ -31,8 +27,7 @@ def get_new_sms():
 		if "Date" in s:
 			smsdate = datetime.strptime(s.split("\t")[2],"%a %b %d %H:%M:%S %Y")
 			timestamp = smsdate.strftime("%Y-%m-%dT%H:%M:%S+02:00")
-			
-			
+						
 	for s in tt:
 		if "Sender" in s:
 			sender = s.split("\t")[2]
@@ -47,8 +42,8 @@ def get_new_sms():
 			
 	return {"timestamp": timestamp, "sender": sender, "status": status, "text": text}
 	
-	
-def send_alarm(alarm, url, week):
+# Senden des Alarms an Server via HTTP POST Request
+def send_alarm(alarm, url):
 	#POST Request erstellen
 	request_data = { 
 		'type': "ALARM",
@@ -58,28 +53,11 @@ def send_alarm(alarm, url, week):
 		'data': { 
 			'message': [
 				alarm["text"]
-			],
-			'custom': {
-				'week': str(week)
-      		}	
+			]
 		}
 	}
-	
 	#Senden Request an FE2 Server
 	r = requests.post(url, json = request_data)
-
-
-#Aktuelle Alarmwoche ermitteln
-def get_week(now, refdates):
-    for refdate in refdates:
-        ref = datetime(refdate[0], refdate[1], refdate[2], 7, 0, 0)    
-        delta = now - ref
-        r = delta.days/42 - math.floor(delta.days/42)
-        if r < 0.166666:
-            return refdates.index(refdate)+1
-
-
-
 
 
 while True:
@@ -87,8 +65,6 @@ while True:
 	if new_sms:
 		alarm = get_new_sms() #Neue SMS abrufen
 		subprocess.check_output(["gsmctl", "-S", "-d", "0"]) #SMS löschen
-		week = get_week(now, refdates) #Wochennummer ermitteln
-		send_alarm(alarm, url, week) # Alarm an FE2 senden
+		send_alarm(alarm, url) # Alarm an FE2 senden
 		
 	time.sleep(1) 
-
